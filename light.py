cc5328bce44f41598af57260fa2bec0d18e79bb0
Himanshu Verma <92078182+himansh-u@users.noreply.github.com>
2022-07-28 18:08:26 +0530

    Update light.py

--- light.py
+++ light.py
@@ -1,3 +1,23 @@
-def leg():
-  a=open("s.txt")
+import pickle
+f=open('student.dat','wb')
+n=int(input("enter the periods"))
+for i in range(n):
+  m=input("enter the marks")
+  g=input("enter the grade")
+  a=input("enter the name")
+  r=input("enter the roll no")
+  d={'marks':m,'grade':g,'name':r,'roll no':d}
+  pickle.dump(f,d)
+f.close()  
+
   
+
+#
+try:
+  while True:
+    f=load.(f)
+    if d["marks"]=="A"
+except:
+  f.close()
+    
+#  
