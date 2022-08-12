c59c27efbb0c648559bb0636c730e14c4428f19a
Himanshu Verma <92078182+himansh-u@users.noreply.github.com>
2022-08-12 16:26:41 +0530

    Create prog3.py

---
+++ prog3.py
@@ -0,0 +1,16 @@
+def count(St):
+  c=0
+  for i in St:
+    if  i ==' ':
+      c=c+1
+  return c
+def count(St):
+  v=c=d=s=0
+  for i in range(len(St)):
+    if St[i].isalpha():
+      if St[i] in "aeiouAEIOU"
+      v=v+1
+      else:
+        c=c+1
+    elif S+[i] in "0123456789"
+    d=d+
