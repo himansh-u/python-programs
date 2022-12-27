0056cd0135bc6987c67bdd9c620c2c4037a42dab
Himanshu Verma <92078182+himansh-u@users.noreply.github.com>
2022-12-27 17:40:26 +0530

    Update purecapitalist.py

--- purecapitalist.py
+++ purecapitalist.py
@@ -5,4 +5,7 @@ def jam():
     print('counting backwards',l)
   else:
     print('counting forward',l)
-jam()  
+jam() 
+
+#ate garam poha while beating to wazir patar's poem and a sunny vibe reel
+#uniting with a lot of friends and just banging on my dashboard 
