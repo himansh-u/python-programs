90c2a13cdbf186d6fd69577ca2a25b7a486a01f0
Himanshu Verma <92078182+himansh-u@users.noreply.github.com>
2022-12-27 17:41:48 +0530

    Update purecapitalist.py

--- purecapitalist.py
+++ purecapitalist.py
@@ -1,11 +1,14 @@
-def jam():
-  list=[]
-  l=input("enter a string")
-  for i in range(10):
-    print('counting backwards',l)
-  else:
-    print('counting forward',l)
-jam() 
-
-#ate garam poha while beating to wazir patar's poem and a sunny vibe reel
-#uniting with a lot of friends and just banging on my dashboard 
+this aint doin nothin just made it for the facts like
+ate samosa 
+singpore's chocolate
+did some lulu rides
+beating jay z and all dat
+being happy and just livin the real artistic life with intellectualism and coolness
+talking to my loves, some socializing 
+bought the fucking pass to the 2 parties, danced a fucking lot
+getting more attached to the reality as it the best when we celebrate
+ate pav bhaji yesterday 
+still nigga life is great.
+ate garam poha while beating to wazir patar's poem and a sunny vibe reel
+uniting with a lot of friends and just banging on my dashboard 
+feeling like eating chickan aachar and my punjabi kudis
