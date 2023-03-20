4d0c1dd408aef516ce29c2c12ee2fc9457e4314d
Himanshu Verma <92078182+himansh-u@users.noreply.github.com>
2023-03-20 21:53:29 +0530

    Update chole.py
    will try to write 1000 lines o code.

--- chole.py
+++ chole.py
@@ -32,14 +32,14 @@ while a:
     a=f.readline()
     print(a)
 f.close()    
-
+#
 f=open("test.txt","r")
 a=" "
 while a:
     a=f.readline()
     print(a.strip())
 f.close()    
-
+#
 X=open('test.txt','r')
 for i in X:
   print(X)
@@ -55,3 +55,45 @@ for i in L:
   s+=1
 f.close()        
   
+#5
+f=open('RESULT.txt','r')
+print('adno','name','total',sep ='\t')  
+for i in f:
+        a=i.split(',')
+        print(a[0],a[1],int(a[2]),int(a[3]),int(a[4]), sep ='\t')
+f.close()        
+#
+f=open('result.txt')
+print('students scoring more than 75 percent are:')
+for i in f:
+        a=split(',')
+        t=0
+for j in range(2,5):
+        t+=int(a[j])
+p=t/3
+if p>75:
+        print(a[1])
+f.close()
+        
+##
+f=open(poem.txt)
+c=c1=0
+words=f.read().split()
+for i in words:
+        if i.lower()=="the":
+             c+=1
+        if i .lower()=="to":
+             c1=c+1
+print("count of word the",c)
+print("count of word to",c1)
+f.close()        
+
+def DISPLAYWORDS():
+    f=open('story.txt')
+    print("those words which are less than char 4:")
+    for word in f.read().split():
+        if len(word)<4:
+            print(word)
+    f.close()            
+ 
+        
